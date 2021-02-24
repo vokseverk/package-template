4c115df87333c5c31f2743bf4f0e63a1d6ca39ea
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-02-25 00:40:01 +0100

    Add stub PropertyValueConverters

---
+++ src/PropertyConverterV7.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Vokseverk {
+	// TODO: Change Type her
+	[PropertyValueType(typeof(string))]
+	[PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+	public class EDITOR_ALIASPropertyConverter : PropertyValueConverterBase {
+		// TODO: Change class name above
+		
+		public override bool IsConverter(PublishedPropertyType propertyType) {
+			// TODO: Change alias
+			return propertyType.PropertyEditorAlias.Equals("EDITOR_ALIAS");
+		}
+		
+		public override object ConvertDataToSource(PublishedPropertyType propertyType, object data, bool preview) {
+			// TODO: Implement
+		}
+	}
+}
---
+++ src/PropertyConverterV8.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Vokseverk {
+	
+	public class KeyValuePropertyConverter : IPropertyValueConverter {
+		// TODO: Change class name above
+		
+		public bool IsConverter(IPublishedPropertyType propertyType) {
+			// TODO: Change alias here
+			return propertyType.EditorAlias.Equals("EDITOR_ALIAS");
+		}
+		
+		public Type GetPropertyValueType(IPublishedPropertyType propertyType) {
+			// TODO: Change Type here
+			return typeof(string);
+		}
+		
+		public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) {
+			return PropertyCacheLevel.Element;
+		}
+		
+		public bool? IsValue(object value, PropertyValueLevel level) {
+			switch (level) {
+				case PropertyValueLevel.Source:
+					// TODO: Implement logic here
+				return value != null && value is string;
+				default:
+					throw new NotSupportedException($"Invalid level: {level}.");
+			}
+		}
+		
+		public object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview) {
+			// TODO: Implement
+		}
+		
+		public object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
+			// TODO: Implement
+		}
+		
+		public object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
+			if (inter == null) {
+				return null;
+			}
+			
+			// TODO: Implement?
+			
+			return inter.ToString();
+		}
+	}
+}
